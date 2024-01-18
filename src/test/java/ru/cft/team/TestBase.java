c12b7bdedc314b6676274c4658c23ecee94ca781
Vlad <s67.freelance@gmail.com>
2024-01-19 02:52:13 +0300

    Обновил Attach и TestBase

--- src/test/java/ru/cft/team/TestBase.java
+++ src/test/java/ru/cft/team/TestBase.java
@@ -3,17 +3,48 @@ package ru.cft.team;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import ru.cft.team.helpers.Attach;
 import ru.cft.team.pages.MainPage;
+
+import java.util.Map;
 
 public class TestBase {
     public MainPage mainPage = new MainPage();
 
     @BeforeAll
     static void setUp() {
+        Configuration.baseUrl = System.getProperty("base_url","https://team.cft.ru");
+        Configuration.remote = System.getProperty("remote_selenoid","http://selenoid:4444/wd/hub");
+        Configuration.browser=System.getProperty("browser","chrome");
+        Configuration.browserSize=System.getProperty("browser_size","1920x1080");
+
+/**
+ * Для записи видео в allure отчете
+ **/
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+    }
+
+    @BeforeEach
+    void addListener() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-        Configuration.baseUrl = "https://team.cft.ru";
-        Configuration.browserSize = "1920x1080";
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+
     }
 }
 
