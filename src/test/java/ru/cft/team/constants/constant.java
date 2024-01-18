3eac00b7650ce14aa019ab7cc919db67934c95e3
Vlad <s67.freelance@gmail.com>
2024-01-19 02:42:28 +0300

    Add costant file

---
+++ src/test/java/ru/cft/team/constants/constant.java
@@ -0,0 +1,7 @@
+package ru.cft.team.constants;
+
+public class constant {
+    public final static String
+            TITLE_TEXT_CENTER_PAGE = "ИТ как искусство",
+            TITLE_MAIN = "Работа и мероприятия в компании Центр Финансовых Технологий (ЦФТ)";
+}
